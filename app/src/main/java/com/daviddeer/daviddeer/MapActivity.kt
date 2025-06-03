af63e8aa633065b09628908221a4365c46a69139
Er-shuang <1343109389@qq.com>
2025-06-03 12:38:56 +0800

    Modified the MainActivity.kt,Remove unnecessary words

--- app/src/main/java/com/daviddeer/daviddeer/MapActivity.kt
+++ app/src/main/java/com/daviddeer/daviddeer/MapActivity.kt
@@ -11,12 +11,7 @@ class MapActivity : ComponentActivity(){
 
         val backButton = findViewById<ImageButton>(R.id.btnBack)
         backButton.setOnClickListener {
-<<<<<<< HEAD
             finish()  // 返回上一界面（MainActivity
-             
-=======
-            finish()  // 返回上一界面（MainActivity）
->>>>>>> fba2954d96ae638b4df89b4d8a48035f0871a533
         }
     }
 }
