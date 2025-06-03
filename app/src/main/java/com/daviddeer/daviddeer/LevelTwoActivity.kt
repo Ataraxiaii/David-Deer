b3ad028a4ebb296039df22c6fa82c9e6dab01a03
Er-shuang <1343109389@qq.com>
2025-06-04 07:11:18 +0800

    The achievement of obstacle clearance and unlocking is permanently stored in the album status.

--- app/src/main/java/com/daviddeer/daviddeer/LevelTwoActivity.kt
+++ app/src/main/java/com/daviddeer/daviddeer/LevelTwoActivity.kt
@@ -25,6 +25,8 @@ class LevelTwoActivity : ComponentActivity() {
     private fun onLevelTwoPassed() {
         // 解锁10~13 的灵兽
         BeastRepository.unlockBeastsByIds(listOf(10, 11, 12, 13))
+        // 每次通关后保存解锁状态
+        BeastRepository.saveUnlockedState(this)
 
         // 提示通关
         Toast.makeText(this, "Successfully passed the level 2!\n4  new beasts have been unlocked.", Toast.LENGTH_SHORT).show()
