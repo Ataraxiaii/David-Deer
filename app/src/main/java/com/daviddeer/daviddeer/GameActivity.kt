fba2954d96ae638b4df89b4d8a48035f0871a533
Er-shuang <1343109389@qq.com>
2025-06-03 12:02:28 +0800

    Add the main interface to jump to the encyclopedia, map, and game interface.

--- app/src/main/java/com/daviddeer/daviddeer/GameActivity.kt
+++ app/src/main/java/com/daviddeer/daviddeer/GameActivity.kt
@@ -1,11 +1,17 @@
 package com.daviddeer.daviddeer
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 
 class GameActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        val backButton = findViewById<ImageButton>(R.id.btnBack)
+        backButton.setOnClickListener {
+            finish()  // 返回上一界面（MainActivity）
+        }
     }
 }
