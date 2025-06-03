7731560f5f9f75e5a00600447fdd118097e2f4c7
Er-shuang <1343109389@qq.com>
2025-06-03 12:02:28 +0800

    Add the functionality to return to the home page from the Bestiary, Map, and Game pages.

--- app/src/main/java/com/daviddeer/daviddeer/BestiaryActivity.kt
+++ app/src/main/java/com/daviddeer/daviddeer/BestiaryActivity.kt
@@ -1,11 +1,17 @@
 package com.daviddeer.daviddeer
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 
 class BestiaryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bestiary)
+
+        val backButton = findViewById<ImageButton>(R.id.btnBack)
+        backButton.setOnClickListener {
+            finish()  // 返回上一界面（MainActivity）
+        }
     }
 }
