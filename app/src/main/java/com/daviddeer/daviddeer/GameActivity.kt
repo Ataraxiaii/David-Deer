26a055ec3859bf4e4b0d4c96db8ad377f2d6f743
Er-shuang <1343109389@qq.com>
2025-06-04 06:52:06 +0800

    Implement the function to jump from the game page to two levels.

--- app/src/main/java/com/daviddeer/daviddeer/GameActivity.kt
+++ app/src/main/java/com/daviddeer/daviddeer/GameActivity.kt
@@ -1,8 +1,12 @@
 package com.daviddeer.daviddeer
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.ComponentActivity
+import com.daviddeer.daviddeer.LevelOneActivity
+import com.daviddeer.daviddeer.LevelTwoActivity
 
 //游戏界面
 class GameActivity : ComponentActivity() {
@@ -13,6 +17,20 @@ class GameActivity : ComponentActivity() {
         val backButton = findViewById<ImageButton>(R.id.btnBack)
         backButton.setOnClickListener {
             finish()  // 返回上一界面（MainActivity）
+        }
+
+        // 跳转到第一关
+        val levelOneButton = findViewById<Button>(R.id.btn_level_one)
+        levelOneButton.setOnClickListener {
+            val intent = Intent(this, LevelOneActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 跳转到第二关
+        val levelTwoButton = findViewById<Button>(R.id.btn_level_two)
+        levelTwoButton.setOnClickListener {
+            val intent = Intent(this, LevelTwoActivity::class.java)
+            startActivity(intent)
         }
     }
 }
